9e6aa4196de40be1151c9ead935af5eca1ef87b6
phuptdot <phupt@dol-tech.com>
2020-05-25 23:52:31 +0700

    fix

--- src/api/index.js
+++ src/api/index.js
@@ -1,3 +1,3 @@
 export default {
-    api: process.env.NODE_ENV === 'development' ? 'http://127.0.0.1:8000/api/' : 'http://tailieu247.net/api/',
+    api: process.env.NODE_ENV === 'development' ? 'http://127.0.0.1:8000/api/' : 'https://tailieu247.net/api/',
 }
